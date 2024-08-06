6b4d13c46aa935c23ec752c47633b969c538725d
Александр Осипов <alikxyzet@gmail.com>
2024-08-06 15:36:46 +0300

    11.0 Base

---
+++ Source/M2PW11_1/M2PW11_1GameModeBase.cpp
@@ -0,0 +1,5 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+
+#include "M2PW11_1GameModeBase.h"
+
---
+++ Source/M2PW11_1/M2PW11_1GameModeBase.h
@@ -0,0 +1,17 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/GameModeBase.h"
+#include "M2PW11_1GameModeBase.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class M2PW11_1_API AM2PW11_1GameModeBase : public AGameModeBase
+{
+	GENERATED_BODY()
+	
+};
